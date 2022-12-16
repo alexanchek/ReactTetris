b0a4461e33fff61493ea844150a8c01e22abc8e2
alexanchek <58663068+alexanchek@users.noreply.github.com>
2022-12-16 14:15:57 +0300

    feat

--- src/components/Tetris/Tetris.tsx
+++ src/components/Tetris/Tetris.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, FC, useState } from "react";
+import { CSSProperties, FC, useState, KeyboardEvent } from "react";
 
 import bgImage from '@assets/bg.png';
 import moduleStyles from './Tetris.module.scss';
@@ -12,6 +12,7 @@ import { Stage } from "../Stage";
 import { Display } from "../Display";
 import { StartButton } from "../StartButton";
 import { ITetrisProps } from "./Tetris.interface";
+import { createStage } from "@utils/gameHelpers";
 
 
 const Tetris: FC<ITetrisProps> = () => {
@@ -19,24 +20,68 @@ const Tetris: FC<ITetrisProps> = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const { player } = usePlayer();
+  const { player, updatePlayerPos, resetPlayer } = usePlayer();
   const { stage, setStage } = useStage();
 
   console.log('re-render')
 
+  const movePlayer = (dir: -1 | 1) => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    // Reset evetything
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ key }: KeyboardEvent) => {
+    if (!gameOver) {
+      if (key === 'ArrowLeft') {
+        movePlayer(-1)
+      }
+
+      if (key === 'ArrowRight') {
+        movePlayer(1);
+      }
+
+      if (key === 'ArrowDown') {
+        dropPlayer();
+      }
+
+    }
+  };
 
   const wrapperStyle: CSSProperties = {
     background: `url(${bgImage}) #000`,
-  }
+  };
 
   return (
-    <div className={moduleStyles.wrapper} style={wrapperStyle}>
+    <div
+      className={moduleStyles.wrapper}
+      style={wrapperStyle}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => move(e)}
+
+    >
       <div className={moduleStyles.tetris}>
         <Stage stage={stage} />
         <aside className={moduleStyles.aside}>
           {gameOver
             ? (
-              <Display gameOver={gameOver} text="Game Over" ></Display>
+              <Display
+                gameOver={gameOver}
+                text="Game Over"
+              />
             ) : (
               <div>
                 <Display text="Score" />
@@ -44,7 +89,7 @@ const Tetris: FC<ITetrisProps> = () => {
                 <Display text="Level" />
               </div>
             )}
-          <StartButton />
+          <StartButton onClick={startGame} />
         </aside>
       </div>
     </div>
--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { randomTetromino } from "@utils/tetrominos";
 
+interface IPlayer {
+  pos: {
+    x: number;
+    y: number;
+  };
+  tetromino: (string | number)[][];
+  collided: boolean;
+}
+
 export const usePlayer = () => {
-  const [player, setPlayer] = useState({
+  const [player, setPlayer] = useState<IPlayer>({
     pos: { x: 0, y: 0 },
     tetromino: randomTetromino().shape,
     collided: false,
   });
 
-  return { player };
+  const updatePlayerPos = ({
+    x, y, collided
+  }: { x: number, y: number, collided: boolean }) => {
+    setPlayer({
+      ...player,
+      pos: {x: player.pos.x += x, y: player.pos.y += y},
+      collided,
+    })
+  };
+
+  const resetPlayer = useCallback(() => {
+    
+  }, [])
+
+  return { player, updatePlayerPos, resetPlayer };
 };
